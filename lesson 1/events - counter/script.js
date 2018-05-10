09d488f0a9ca71dd7183582bc280253dc2ed1304
Marcin Zaborowski <jimrajnor@gmail.com>
2018-05-10 12:00:36 +0000

    do clean up

---
+++ lesson 1/events - counter/script.js
@@ -0,0 +1,26 @@
+var Vue
+
+new Vue({
+	el: '#app',
+  data: {
+  	counter: 0,
+  	x: 0,
+  	y: 0,
+  	add: 1
+  },
+  methods: {
+    addChange: function (event) {
+        this.add = event.target.value
+    },
+  	increase: function(add) {
+  	    this.counter += Number(add)
+    },
+    updateCoordinates: function (event) {
+        this.x = event.clientX
+        this.y = event.clientY
+    },
+    alertMe: function (event) {
+        alert(event.target.value)
+    }
+  }
+})
