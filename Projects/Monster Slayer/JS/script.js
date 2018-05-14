fd674c2da02966582582c3a4425438169d61b6e2
Marcin Zaborowski <jimrajnor@gmail.com>
2018-05-14 18:40:57 +0000

    add first skill

--- Projects/Monster Slayer/JS/script.js
+++ Projects/Monster Slayer/JS/script.js
@@ -6,20 +6,50 @@ new Vue({
     isRunning: false,
     player: {
       name: 'Thrajnor',
-      currentHealth: 80,
+      health: 125,
       maxHealth: 125,
-      weapon: 10
+      weapon: 10,
+      stun: false,
+      skills: {
+        skill1: 'Attack',
+        skill2: 'Strong Attack',
+        skill3: 'Heal'
+      }
     },
     monster: {
       name: 'Goblin',
-      currentHealth: 55,
+      health: 100,
       maxHealth: 100,
-      damage: 10
+      damage: 10,
+      stun: false,
+      skills: {
+        skill1: 'Stab',
+        skill2: 'Swith Blow',
+        skill3: 'Potion',
+      }
     },
     log: {
       
     }
   },
   methods: {
+    attack: function(wait) {
+      if (!wait) {
+        var damage = this.player.weapon + Math.round(6 * ((Math.random() * 1000) / 999.99))
+        this.monster.health -= damage
+        return this.nextTurn('monster')
+      } else {
+        return this.nextTurn('monster')
+      }
+    },
+    strongAttack: function(wait) {
+      
+    },
+    heal: function(wait) {
+      
+    },
+    nextTurn: function (round) {
+      
+    }
   }
 });
